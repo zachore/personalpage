46942f0cd69ad0fc4a1d674d799f78116121e43a
Zach Orelowitz <Zach@Zachs-MacBook-Pro.local>
2017-06-14 16:07:16 +0200

    added package.json

--- anything.js
+++ anything.js
@@ -33,3 +33,13 @@ $('a[href*="#"]')
       }
     }
   });
+
+  $("#nav").addClass("js").before('<div id="menu">&#9776;</div>');
+	$("#menu").click(function(){
+		$("#nav").toggle();
+	});
+	$(window).resize(function(){
+		if(window.innerWidth > 768) {
+			$("#nav").removeAttr("style");
+		}
+	});
